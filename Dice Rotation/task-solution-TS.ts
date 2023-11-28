48b0c29025c2f426d82627829ace7bc4b55fb374
Aleksandr_K <a.v.kleriv@gmail.com>
2023-11-28 14:54:40 +0300

    Added a solution for one task

---
+++ Dice Rotation/task-solution-TS.ts
@@ -0,0 +1,9 @@
+export const rotations = (dieArray: number[]): number => {
+  const dict: number[] = [5, 4, 3, 2, 1, 0];
+  return Math.min(
+    ...dieArray.reduce(
+      (c, s) => c.map((v, i) => v + +(dict[s - 1] === i) + +(i !== s - 1)),
+      [0, 0, 0, 0, 0, 0],
+    ),
+  );
+};
