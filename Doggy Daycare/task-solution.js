7aba5464b479c18276906b6f072c93c5b8203595
Aleksandr_K <a.v.kleriv@gmail.com>
2023-12-09 16:11:32 +0300

    Added solutions for three tasks

---
+++ Doggy Daycare/task-solution.js
@@ -0,0 +1,7 @@
+//extend the dog prototype with the checkDog method.
+
+Dog.prototype.checkDog = function () {
+  if (this.vaccinated && this.wormed) return `${this.name} can be accepted`;
+  if (this.vaccinated || this.wormed) return `${this.name} can only be accepted by itself`;
+  return `${this.name} can not be accepted`;
+};
