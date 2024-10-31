bf28aebd651b20c849a0d809fe4dd8fde2f53836
AKleriX <a.v.kleriv@gmail.com>
2024-10-31 18:45:17 +0100

    Added a solution for a task

--- Perfect squares, perfect fun/task-solution.js
+++ Perfect squares, perfect fun/task-solution.js
@@ -1,9 +1,18 @@
-const squareIt = (int) => {
-  const sq = Math.sqrt(int.toString().length);
-  return sq % 1
-    ? 'Not a perfect square!'
-    : int
-        .toString()
-        .match(new RegExp('.{' + sq + '}', 'g'))
-        .join('\n');
+const lastNonEmptyString = str => {
+  let count = new Array(26).fill(0),
+      l = new Array(26).fill(0),
+      n = str.length,
+      max = 0;
+  for (let i = 0; i < n; ++i) {
+    let c = str.charCodeAt(i) - 'a'.charCodeAt(0);
+    max = Math.max(max, ++count[c]);
+    l[c] = i;
+  }
+  let ans = '';
+  for (let i = 0; i < n; ++i) {
+    let c = str.charCodeAt(i) - 'a'.charCodeAt(0);
+    if (count[c] === max && l[c] === i)
+      ans += str[i];
+  }
+  return ans;
 };
